264a20b5e07f15ed5a571fc584a3680c204264a5
Daksh Patel <dakshpatel1700@gmail.com>
2025-06-17 12:16:08 -0400

    Home Page Updates

--- react-folder/src/pages/Home.jsx
+++ react-folder/src/pages/Home.jsx
@@ -4,7 +4,8 @@ import '../css/Home.css'
 function Home() {
     return (
         <div className="home">
-         <h1>HOME PAGE</h1>
+         <h1>Welcome to Navigate EECS @ Lassonde School of Engineering</h1>
+         <h2>Choose your Program Below</h2>
         </div>
     )
 }
