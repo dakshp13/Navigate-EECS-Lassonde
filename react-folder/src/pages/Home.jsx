692c75d51a5eab20b63ccd6e665e90c24d468a5a
Daksh Patel <dakshpatel1700@gmail.com>
2025-06-17 19:13:34 -0400

    Finished Home-Page Component

--- react-folder/src/pages/Home.jsx
+++ react-folder/src/pages/Home.jsx
@@ -9,9 +9,14 @@ function Home() {
          <h1>Welcome to Navigate EECS @ Lassonde School of Engineering</h1>
          <h2>Choose your Program Below</h2>
          <div className="actions">
+          <ProgramButton name="First-Year Common Engineering" />
           <ProgramButton name="Software Engineering" />
           <ProgramButton name="Computer Engineering" />
           <ProgramButton name="Electrical Engineering" />
+          <p>Notes: All engineering students follow a common schedule in their first year so all the material can be
+            found via the first button. The other program specific buttons only include information for second year
+            and onwards.
+          </p>
          </div>
         
         </div>
