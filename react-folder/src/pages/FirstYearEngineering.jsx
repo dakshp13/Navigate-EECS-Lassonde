fd476b91688ab66930a17789007cc588835bce4e
Daksh Patel <dakshpatel1700@gmail.com>
2025-06-18 15:17:41 -0400

    Changed Up FYE UI and added Notes

--- react-folder/src/pages/FirstYearEngineering.jsx
+++ react-folder/src/pages/FirstYearEngineering.jsx
@@ -7,18 +7,19 @@ function FirstYearEngineering(){
         <h1>First Year Engineering Page</h1>
         <h2>Below are the Course you will take in First Year</h2>
         <div className="actionsFirstYear">
-            <CourseButtons name="MATH 1013: Applied Calculus 1" />
-            <CourseButtons name="MATH 1014: Applied Calculus 2" />
-            <CourseButtons name="MATH 1025: Applied Linear Algebra" />
-            <CourseButtons name="MATH/EECS 1028: Discrete Mathematics for Engineers" />
-            <CourseButtons name="PHYS 1800: Engineering Mechanics" />
-            <CourseButtons name="PHYS 1801: Electricity, Magnetism and Optics for Engineers" />
-            <CourseButtons name="CHEM 1100: Chemistry and Materials Science for Engineers" />
-            <CourseButtons name="EECS 1011: Computational Thinking Through Procedural Programming and Mechatronics" />
-            <CourseButtons name="EECS 1021: Object Oriented Programming from Sensors to Actuators" />
-            <CourseButtons name="ENG 1101: Renaissance Engineering 1: Ethics, Communication and Problem Solving" />
-            <CourseButtons name="ENG 1102: Renaissance Engineering 2: Engineering Design Principles" />
+            <CourseButtons name="MATH 1013" />
+            <CourseButtons name="MATH 1014" />
+            <CourseButtons name="MATH 1025" />
+            <CourseButtons name="MATH 1028" />
+            <CourseButtons name="PHYS 1800" />
+            <CourseButtons name="PHYS 1801" />
+            <CourseButtons name="CHEM 1100" />
+            <CourseButtons name="EECS 1011" />
+            <CourseButtons name="EECS 1021" />
+            <CourseButtons name="ENG 1101" />
+            <CourseButtons name="ENG 1102" />
         </div>
+        <p>Note: MATH 1028 is the same (cross-listed) as EECS 1028</p>
        </div>
     )
 }
