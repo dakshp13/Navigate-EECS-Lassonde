f79c2beb160318e3c652095ed87cc8095a35d6fe
Daksh Patel <dakshpatel1700@gmail.com>
2025-06-19 19:48:26 -0400

    FireBase Implementation Complete

--- react-folder/src/pages/Home.jsx
+++ react-folder/src/pages/Home.jsx
@@ -1,9 +1,38 @@
 import '../css/Home.css'
 import '../components/ProgramButtons'
 import ProgramButton from '../components/ProgramButtons'
+import { useState } from "react";
+import { db } from "../firebase"; 
+import { collection, addDoc } from "firebase/firestore";
+
+
+
 
 
 function Home() {
+
+  const [review, setReview] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!review.trim()) {
+      alert("Please write a review first!");
+      return;
+    }
+    try {
+      await addDoc(collection(db, "reviews"), {
+        review: review,
+        createdAt: new Date(),
+      });
+      alert("Review submitted successfully!");
+      setReview(""); 
+    } catch (error) {
+      console.error(error);
+      alert("Error submitting review. Try again later.");
+    }
+  };
+  
+  
     return (
         <div className="home">
          <h1>Welcome to Navigate EECS @ Lassonde School of Engineering</h1>
@@ -12,10 +41,22 @@ function Home() {
           <ProgramButton name="First-Year Common Engineering" />
           <ProgramButton name="Software Engineering: Core Courses" />
           <ProgramButton name="Software Engineering: Big Data Stream Courses" />
-          <p>Notes: All engineering students follow a common schedule in their first year so all the material can be
+          <p id="Notes">Notes: All engineering students follow a common schedule in their first year so all the material can be
             found via the first button. The other program specific buttons only include information for second year
             and onwards.
           </p>
+          <h3 id="ReviewText">Feel Free to Leave a Review Below: Can be anything in terms of new resources or just other advice to add</h3>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+              placeholder="Enter your review here..."
+              className="review-box"
+            />
+            <button id="SubmitButton" type="submit">Submit Review</button>
+          </form>
+        
+ 
          </div>
         
         </div>
