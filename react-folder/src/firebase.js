39a4d4617b0c9dee7529f6b6e9ad2074feea8a37
Daksh Patel <dakshpatel1700@gmail.com>
2025-11-02 21:47:19 -0500

    Check

--- react-folder/src/firebase.js
+++ react-folder/src/firebase.js
@@ -17,6 +17,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+console.log("VITE_PROJECT_ID:", import.meta.env.VITE_PROJECT_ID);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
